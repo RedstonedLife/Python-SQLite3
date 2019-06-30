69058c82a3446eb9818dac55a4529e5dfa871aa7
RedstonedLife <37072229+RedstonedLife@users.noreply.github.com>
2019-06-30 14:44:31 +0300

    Update DBManager.py

--- 1-file-database/DBManager.py
+++ 1-file-database/DBManager.py
@@ -3,17 +3,16 @@ import sqlite3
 database = "PATH/TO/DATABASE.db" # Note doesn't have to be complete path, It can also be just FOLDER_CONTAINING_DATABSE/database.db
 # If there is no such folder then just type database.db
 
-def createTables():
+def createTables(sql_statement = None):
   db = sqlite3.connect(database)
   
   # To generate table creation statements you can use https://editor.datatables.net/generator/ | Its a very useful website, I'm using it from time to time
   # I suggest using 'IF NOT EXISTS' statement that will create sayed table if it doens't exist in sayed file.
   
   
-  sql_statement = '''CREATE TABLE IF NOT EXISTS `TABLENAME` (
-	  `id` int(10) NOT NULL,
-	  PRIMARY KEY( `id` )
-  );'''
+  if not sql_statement:
+	print("Please input a sql statement to create tables")
+	return
   
   cursor = db.cursor() # Creates a "Cursor" to read & write data if required
   
@@ -25,7 +24,7 @@ def createTables():
   
 def change(table,user,value,newValue):
   db = sqlite3.connect(database) # Connects to database
-  
+  cursor = db.cursor()
   # user needs to be the id of said user (e.g: 59654301232)
   
   # value should be the name of the value (e.g: 'cash')
@@ -34,4 +33,10 @@ def change(table,user,value,newValue):
   # for the value, If its varchar(255) give it a string, if its int(9-999) or numeric(9,2) give it numbers
   
   # table should be the name of the table the data is saved on
-  
+  sql_statement = '''UPDATE '''+str(table)+''' SET '''+str(value)+'''='''+str(newValue)+''' WHERE id='''+str(user)+''';'''
+
+  cursor.execute(sql_statement)
+  db.commit()
+  print("Successfuly updated {} {} to {}".format(user,value,newValue))
+  return
+
